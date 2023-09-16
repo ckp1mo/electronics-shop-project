4c7ed23148861fd62f9c9333f91f705425ff9b92
Alexandr Gritsunov <ickpimo@gmail.com>
2023-09-16 21:25:49 +0300

    hw 2 completed

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +16,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
@@ -31,3 +34,39 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * Item.pay_rate
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """
+        Проверка длины имени и если больше 10 символов, то режет
+        """
+        if len(name) <= 10:
+            self.__name = name
+        else:
+            self.__name = name[0:10]
+
+    @classmethod
+    def instantiate_from_csv(cls, file):
+        """
+        Собирает экземпляры класса на основе содержимого файла csv.
+        Очищает список ранее добавленных экземпляров
+        """
+        Item.all.clear()
+        with open(file, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                name = row['name']
+                price = int(row['price'])
+                quantity = int(row['quantity'])
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(str_number):
+        """
+        Переводит строковое значение в числовое, отбросив цифры после точки
+        """
+        return int(float(str_number))
