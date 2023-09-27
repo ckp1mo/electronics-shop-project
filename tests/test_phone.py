8ee0ccab6f743abf6fab123131e80d088cc42ce4
Alexandr Gritsunov <ickpimo@gmail.com>
2023-09-27 10:55:31 +0300

    tests for subclass Phone

---
+++ tests/test_phone.py
@@ -0,0 +1,19 @@
+import pytest
+
+from src.phone import Phone
+
+
+@pytest.fixture
+def iphone():
+    return Phone('iPhone XR', 32000, 15, 1)
+
+
+def test_repr(iphone):
+    assert repr(iphone) == "Phone('iPhone XR', 32000, 15, 1)"
+
+
+def test_number_of_sim(iphone):
+    with pytest.raises(ValueError):
+        Phone('iPhone XR', 32000, 15, 0)
+
+
